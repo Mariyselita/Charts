4dde21e6828c08f4bcc7205261d923119a96bc49
marisela Cadena <mariyselita@MacBook-Pro-de-marisela.local>
2019-11-23 17:56:24 -0600

    Esqueleto para deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Chinga tu madre rUBEN :V"/>
-  </div>
+<div id="app">
+   <header>
+        <nav class="menu" id="myTopnav">
+            <ul>
+                <li style="float:left;">Bayonet</li>
+                <li><a href="">Column Chart</a></li>
+                <li><a href="">Line Chart</a></li>
+            </ul>          
+        </nav>
+    </header>
+    <home/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="forma">
+        <div>
+            <h1>Data Analysis using Github API</h1>
+            <a href="#" class="misBotones">search repositories</a>
+            <a href="#" class="misBotones">statistics endpoint</a>
+         
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  
+}
+</script>
+
