7adef758410ebbe3b8ee7424e5a0d76b26fc5091
marisela Cadena <mariyselita@MacBook-Pro-de-marisela.local>
2019-11-23 22:33:09 -0600

    ChartLineal pre

--- src/components/Commit.vue
+++ src/components/Commit.vue
@@ -1,11 +1,32 @@
 <template>
-<div>
-<h1>ola ke ase Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium dolorum, nesciunt consequuntur ad vero aspernatur iste consectetur, voluptas aperiam cumque molestiae veniam, aut libero. Nostrum numquam cum ab a vero?</h1>
-</div>
+    <line-chart :chart-data="commitReactData"></line-chart>
 </template>
 
 <script>
+import LineChart from './LineChart.vue';
 export default {
-  
+   components: { LineChart },
+   data () {
+      return {
+        commitReactData: {}
+      }
+    },
+    async created(){
+         const response = await this.axios.get('https://api.github.com/repos/facebook/react/stats/commit_activity', {
+            headers: { 'Accept':'application/vnd.github.v3+json'}
+        });
+        if (Array.isArray(response.data)&& response.data.length >= 10){
+
+            this.commitReactData = {
+                labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10'],
+                datasets: [{ 
+                    label: 'React Commit Activity',
+                    borderColor: '#72c7DE',
+                    data: response.data.slice(-10).map(week => week.total)
+                }],
+                
+            };
+        } 
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
 
 import routes from './routes'
 
 import './assets/css/style.css'
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
 
 const router = new VueRouter({routes});
 
